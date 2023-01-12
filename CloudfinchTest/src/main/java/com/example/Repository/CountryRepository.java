03d9cd31622787dcc5daa67e2bcd9cdd5cc2aeba
Priya <sharma27598@gmail.com>
2023-01-12 13:04:03 +0530

    cloudfinch commit 2

--- CloudfinchTest/src/main/java/com/example/Repository/CountryRepository.java
+++ CloudfinchTest/src/main/java/com/example/Repository/CountryRepository.java
@@ -2,6 +2,14 @@ package com.example.Repository;
 
 import com.example.Entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface CountryRepository extends JpaRepository<Country, String> {
+
+    @Query("select continent, name, area from world WHERE name IN (SELECT continent, name, MAX(area) FROM world GROUP BY continent")
+    public List<Country> getAllLargestCountry();
 }
